55937ec555b58967ad87d4289ce4447922c74c36
NGL <hatimsami555@gmail.com>
2026-02-02 17:04:56 +0100

    change: some trouble diagnostics keymaps

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -6,11 +6,50 @@ return {
   },
   cmd = 'Trouble',
   keys = {
-    -- Use these to see errors in your C/PHP projects
-    { '<leader>dx', '<cmd>Trouble diagnostics toggle<CR>', desc = 'Project Diagnostics (Trouble)' },
-    { '<leader>db', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>', desc = 'Buffer Diagnostics (Trouble)' },
-    { '<leader>dq', '<cmd>Trouble quickfix toggle<CR>', desc = 'Quickfix List (Trouble)' },
-    { '<leader>dl', '<cmd>Trouble loclist toggle<CR>', desc = 'Location List (Trouble)' },
-    { '<leader>dt', '<cmd>Trouble todo toggle<CR>', desc = 'Open Todos in Trouble' },
+    -- PROJECT-WIDE: See every error in your whole project
+    { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Project)' },
+
+    -- BUFFER-ONLY: Focus only on the PHP/C file you are currently editing
+    { '<leader>xb', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Diagnostics (Buffer)' },
+
+    -- SYMBOLS: Great for jumping between functions in large systems/C projects
+    { '<leader>xs', '<cmd>Trouble symbols toggle focus=false<cr>', desc = 'Symbols (LSP)' },
+
+    -- TODOs: View all your "TODO", "FIXME", or "HACK" comments
+    { '<leader>xt', '<cmd>Trouble todo toggle<cr>', desc = 'Todo Comments' },
+
+    -- LISTS: Standard quickfix and location lists
+    { '<leader>xq', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List' },
+    { '<leader>xl', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List' },
+
+    -- JUMPING: Jump to the next error without opening a menu
+    {
+      '[q',
+      function()
+        if require('trouble').is_open() then
+          require('trouble').prev { skip_groups = true, jump = true }
+        else
+          local ok, err = pcall(vim.cmd.cprev)
+          if not ok then
+            vim.notify(err, vim.log.levels.ERROR)
+          end
+        end
+      end,
+      desc = 'Previous Trouble/Quickfix Item',
+    },
+    {
+      ']q',
+      function()
+        if require('trouble').is_open() then
+          require('trouble').next { skip_groups = true, jump = true }
+        else
+          local ok, err = pcall(vim.cmd.cnext)
+          if not ok then
+            vim.notify(err, vim.log.levels.ERROR)
+          end
+        end
+      end,
+      desc = 'Next Trouble/Quickfix Item',
+    },
   },
 }
