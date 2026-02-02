cf7add8f3095d22ac29f93af1d466be61955d3f9
NGL <hatimsami555@gmail.com>
2026-02-02 17:12:57 +0100

    add: phpstan lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -7,6 +7,7 @@ return {
       python = { 'ruff' },
       bash = { 'shellcheck' },
       make = { 'checkmake' },
+      php = { 'phpstan' },
     }
 
     local augroup = vim.api.nvim_create_augroup('lint', { clear = true })
